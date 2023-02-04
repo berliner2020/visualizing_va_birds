ad03b163bcad7b1df22701e843dab6fed92f5430
Jeremy Lykken <jlykken@kodinggriz.awsapps.com>
2023-02-04 14:23:37 -0500

    move request functions into ebird_request.py

--- main.py
+++ main.py
@@ -1,22 +1,45 @@
 import config
 import requests
-import pandas
+import json
+import pandas as pd
 import dash
 
+# Setup
 eBirdAPIkey = config.eBirdAPIkey
 
+payload = {}
 headers = {
-    "Accept": "application/json",
-    "x-ebirdapitoken": eBirdAPIkey,
-    "Accept-Encoding": "gzip, deflate, br",
-    "Connection": "keep-alive",
-    "Cache-Control": "max-age=0"
+            "Accept": "application/json",
+            "x-ebirdapitoken": eBirdAPIkey,
+            "Connection": "keep-alive",
+            "Cache-Control": "max-age=0"
            }
 
-r = requests.get(
-    "https://api.ebird.org/v2/data/obs/VA/recent",
-    headers=headers
-)
+# Models
+# Get subregion information for US-VA
+# subregion = "US-VA"
+# va_subregions = requests.request(
+#     "GET",
+#     url=f"https://api.ebird.org/v2/ref/region/list/subnational2/{subregion}",
+#     headers=headers
+# )
+#
+# print(va_subregions.json())
 
-print(r.json())
+
+
+
+
+
+
+# r = requests.get(
+#     "https://api.ebird.org/v2/data/obs/VA/recent",
+#     headers=headers
+# )
+
+# region = requests.get(
+#     "https://api.ebird.org/v2/ref/region/info/US",
+#     headers=headers
+# )
+
 
